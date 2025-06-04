455effeb20bffb6ce79af48dafc5852a9bdbe0c3
Theangel256 <angel.amarillas@proton.me>
2025-06-03 22:20:20 -0700

    final webpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/delirina-margarina/',
   vite: {
       plugins: [tailwindcss()],
   },
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,7 @@ export const personalInfo = {
   name: "Delirina Margarina",
   location: "Hermosillo, Sonora",
   email: "delirinaventashmo@gmail.com",
-  phone: "https://wa.me/+526624444354",
+  phone: "https://wa.me/+526624444354?text=*Hola,%20Me%20gustar%C3%ADa%20saber%20mas%20informacion%20sobre%20su%20producto%20de%20mantequilla%20Delirina!%20*%E2%98%BA%EF%B8%8F",
 };
 export type ImageItem = {
   title: string;
